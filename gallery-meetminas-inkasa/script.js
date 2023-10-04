20fa1fe7dadd7e3b1682dd1bc0c61caebeda0fce
Amsterdan Vasconcelos <135554841+Amsterdanvasconcelos@users.noreply.github.com>
2023-10-04 09:38:40 -0300

    terminado segunda parte do script - gallery-meetminas
    Add script para update ao modal ao clicar nas imagens da galeria.
    E update do modal ao clicar nas imagens das thumbs-modal

---
+++ gallery-meetminas-inkasa/script.js
@@ -0,0 +1,86 @@
+const galleryItems = document.querySelectorAll('[data-gallery="gallery-item"]')
+const modal = document.querySelector('[data-modal="modal"]')
+const btnClose = document.querySelector('[data-modal="btn-close"]')
+const sliderImg = document.querySelector('[data-modal="slider-img"]')
+const sliderDescription = document.querySelector('[data-modal="slider-description"]')
+const sliderNumber = document.querySelector('[data-modal="slider-number"]')
+const thumbImgs = document.querySelectorAll('[data-modal="thumb-img"]')
+
+const gallery = [
+	{
+		img: "img/photo_0.png",
+		description: "Conceição do Mato Dentro"
+	},
+	{
+		img: "img/photo_1.png",
+		description: "Diamantina"
+	},
+	{
+		img: "img/photo_2.png",
+		description: "Tiradentes"
+	},
+	{
+		img: "img/photo_3.png",
+		description: "São Lourenço"
+	},
+	{
+		img: "img/photo_4.png",
+		description: "Serro"
+	},
+	{
+		img: "img/photo_5.png",
+		description: "São Tomé das Letras"
+	},
+	{
+		img: "img/photo_6.png",
+		description: "Ipoema"
+	},
+	{
+		img: "img/photo_7.png",
+		description: "Ouro Preto"
+	}
+]
+
+const showModal = () => modal.style.display = 'flex'
+
+const closeModal = () => modal.style.display = 'none'
+
+const getImgIndex = ({ target }) => {
+    const arrFromChildren = Array.from(target.parentNode.children)
+    const indexImg = arrFromChildren.indexOf(target)
+    
+    return indexImg
+}
+
+const thumbActive = (index) => {
+    thumbImgs.forEach(img => {
+        img.classList.remove('c-modal__thumb--active')
+    })
+    
+    thumbImgs[index].classList.add('c-modal__thumb--active')
+}
+
+const updateModal = (index) => {
+    sliderImg.src = gallery[index].img
+    sliderDescription.textContent = gallery[index].description
+    sliderNumber.textContent = (index + 1) + ' / ' + gallery.length
+
+    thumbActive(index)
+}
+
+btnClose.addEventListener('click', closeModal)
+
+galleryItems.forEach(item => {
+    item.addEventListener('click', event => {
+        const indexImg = getImgIndex(event)
+        updateModal(indexImg)
+        showModal()
+    })
+})
+
+thumbImgs.forEach(img => {
+    img.addEventListener('click', event => {
+        const indexImg = getImgIndex(event)
+        updateModal(indexImg)
+    })
+})
